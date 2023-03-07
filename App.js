88ef090ab301dc9cc1622faad05acb4b6bf87022
Axel Adán <82789056+AxelAdan@users.noreply.github.com>
2023-03-06 19:02:49 -0600

    xd

--- App.js
+++ App.js
@@ -1,4 +1,4 @@
-/*import React from 'react';
+import React from 'react';
 import * as eva from '@eva-design/eva';
 import { ApplicationProvider, IconRegistry } from '@ui-kitten/components';
 import HomePage from './Screens/HomePage';
@@ -12,17 +12,4 @@ export default () => (
       <HomePage />
     </ApplicationProvider>
   </>
-);*/
-
-// hola mundo con tailwind
-import React from 'react';
-import { View, Text } from 'react-native';
-import tailwind from 'tailwind-rn';
-
-export default function App() {
-  return (
-    <View style={tailwind('bg-blue-500 h-full')}>
-      <Text style={tailwind('text-white text-2xl font-bold')}>Hola mundo</Text>
-    </View>
-  );
-}
+);
